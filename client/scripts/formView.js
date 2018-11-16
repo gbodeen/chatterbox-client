9001563b243f63ac2ef9a5d8c70903da37f74618
gbodeen <gabebodeen@gmail.com>
2018-11-16 14:36:23 -0600

    POST a message with submit button.

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -2,18 +2,27 @@ var FormView = {
 
   $form: $('form'),
 
-  initialize: function() {
+  initialize: function () {
     FormView.$form.on('submit', FormView.handleSubmit);
   },
 
-  handleSubmit: function(event) {
+  handleSubmit: function (event) {
     // Stop the browser from submitting the form
     event.preventDefault();
-    
-    console.log('click!');
+
+    let message = {
+      username: 'CornerTeam',
+      text: '',
+      roomname: 'Lobby'
+    };
+
+    message.text = $('#message').val();
+
+    Parse.create(message);
+    console.log('click!', message);
   },
 
-  setStatus: function(active) {
+  setStatus: function (active) {
     var status = active ? 'true' : null;
     FormView.$form.find('input[type=submit]').attr('disabled', status);
   }
